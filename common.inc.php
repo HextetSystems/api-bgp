1dc2d733923b50a476e15c77125fdf67f0a50bab
Theodore Baschak <theodore@ciscodude.net>
2016-06-09 11:34:20 -0500

    add ASN name lookup

--- common.inc.php
+++ common.inc.php
@@ -20,7 +20,7 @@ function info_by_reverse($dnslookup) {
     // 14061 | 2604:a880:1::/48 | US | arin | 2013-04-11
     $txt = $result[0]['txt'];
     list($asn, $prefix, $bgpcountry, $region, $somedate) = explode(" | ", $txt);
-    echo "prefix $prefix advertised by $asn\n";
+    echo "prefix $prefix advertised by $asn ($asname)\n";
   }
 }
 function asn_by_reverse($dnslookup) {
@@ -39,6 +39,21 @@ function prefix_by_reverse($dnslookup) {
     $txt = $result[0]['txt'];
     list($asn, $prefix, $bgpcountry, $region, $somedate) = explode(" | ", $txt);
     echo "$prefix\n";
+  }
+}
+function asnname($asnumber) {
+  //AS23028.asn.cymru.com
+  $dnslookup = 'AS' . (int)$asnumber . '.asn.cymru.com.';
+  $result = dns_get_record($dnslookup, DNS_TXT);
+  if($result[0]['txt']) {
+    // 23028 | US | arin | 2002-01-04 | TEAMCYMRU - SAUNET
+    $txt = $result[0]['txt'];
+    list($asn, $bgpcountry, $region, $somedate, $asndesc) = explode(" | ", $txt);
+    list($null1, $asndesc2) = explode(" - ", $asndesc);
+//    list($asndesc3, $null2) = explode(", ", $asndesc2);
+    return($asndesc2);
+  } else {
+    return "";
   }
 }
 ?>
